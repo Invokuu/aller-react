e814def3aefe4a92dd9c3402cf9e005db111d4ac
Andy Gerhard <andy@invoku.net>
2022-05-23 10:48:14 +0200

    updated favicon and title

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,12 @@ import {ResponsiveContainer} from './ResponsiveContainer';
 import './scss/Footer.scss';
 
 // The footer used globally on the page
-export function Footer(props: any): JSX.Element {
+export function Footer(): JSX.Element {
     return (
         <footer className="page-footer">
             <ResponsiveContainer>
                 <p>Made by Andy</p>
+                <p>Not an official <a href="https://dagbladet.no/" target="_blank" rel="noreferrer">Dagbladet</a> site</p>
             </ResponsiveContainer>
         </footer>
     );
